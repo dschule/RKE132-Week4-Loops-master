ff453e08f5cc0a6109c3a65c4e56abed3bebffd8
dschule <delen.schule@gmail.com>
2025-05-15 16:14:26 +0300

    Add files via upload

---
+++ RKE132-Week4-Loops-master/WhileLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true; //bool - true/false
+
+while(loopActive)
+{
+    cpuRandom = rnd.Next(1, 4); //1 - 3
+    //Console.WriteLine($"cpu has generated {cpuRandom}.");
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+    
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        break;//loopActive = false;
+    }
+    else
+    {
+        Console.WriteLine("Opps. Try again.");
+    }
+}
+
+Console.WriteLine("Have a nice day!");
